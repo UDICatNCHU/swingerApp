3135399865172826319215f896dadffe6db99edb
david <davidtnfsh@gmail.com>
2017-04-18 16:57:12 +0800

    load swinger object when request swinger api

--- swingerApp/views.py
+++ swingerApp/views.py
@@ -2,13 +2,14 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from Swinger import Swinger
-# Create your views here.
-s = Swinger()
-# 讀取方式2選1
-s.load('NuSVC') # 讀取預設模型
 
 def swing(request):
 	if request.POST:
+		# Create your views here.
+		s = Swinger()
+		# 讀取方式2選1
+		s.load('NuSVC') # 讀取預設模型
+		
 		post = request.POST
 		post = post.dict()
 		return JsonResponse({'result':s.swing(post['sentence'])}, safe=False)
